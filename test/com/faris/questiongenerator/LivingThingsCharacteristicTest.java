a97d8ee1066347b9fe5e2058165fd6a18c2a65f1
Faris Hafizhan Hakim <farishafiz999@gmail.com>
2018-10-28 14:08:25 +0700

    First working prototype of biology

---
+++ test/com/faris/questiongenerator/LivingThingsCharacteristicTest.java
@@ -0,0 +1,11 @@
+package com.faris.questiongenerator;
+
+import org.junit.Test;
+
+public class LivingThingsCharacteristicTest {
+
+    @Test
+    public void generate() {
+        new LivingThingsCharacteristic().generate();
+    }
+}
